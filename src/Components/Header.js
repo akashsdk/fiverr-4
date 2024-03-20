6ff77fcce4b270f5b9bb44580e6d00927601a456
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-21 04:48:38 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Header.css';
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='Header-Body'>
+      <div>
+        <Link to='/'><p style={{fontSize:'30px'}}>Shop Name</p></Link>
+      </div>
+
+      <div>
+        ho ho ho ho ho ho ho ho ho ho ho ho
+      </div>
+    </div>
   )
 }
--- src/Screen/Testimonials.js
+++ src/Screen/Testimonials.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Header from '../Components/Header'
 
 export default function Testimonials() {
   return (
-    <div>Testimonials</div>
+    <Header></Header>
   )
 }
