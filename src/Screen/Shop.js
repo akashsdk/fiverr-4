4c14fc304072c679ad96f979242df22dc9711aed
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-22 02:27:24 +0600

    ok

--- src/Screen/Shop.js
+++ src/Screen/Shop.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Shop.css";
 
 export default function Shop() {
   return (
-    <div>Shop</div>
-  )
+    <div className="Shop-Body">
+      <div className="Shop-Left">
+        <div className="Shop-Search-Div">
+          <input className="Shop-Input" placeholder="Search Products..." />
+          <button className="Shop-Search-Button">
+            <p className="Shop-Search-Text">Search</p>
+          </button>
+        </div>
+
+        <p>Categories</p>
+
+        <div>
+          <button>
+            <p>Bonsai</p>
+            <p>[4]</p>
+          </button>
+
+          <button>
+            <p>Cactus</p>
+            <p>[4]</p>
+          </button>
+
+          <button>
+            <p>Indoor Plants</p>
+            <p>[5]</p>
+          </button>
+
+          <button>
+            <p>Succulent</p>
+            <p>[4]</p>
+          </button>
+        </div>
+
+        <p>Filter By Price</p>
+      </div>
+
+      <div className="Shop-Right">Right</div>
+    </div>
+  );
 }
