4d2819fdf4b58d3ca7f908c4a040b22779a6d495
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-25 05:30:45 +0600

    ok

--- src/Screen/AboutUs.js
+++ src/Screen/AboutUs.js
@@ -10,19 +10,25 @@ import {
   YoutubeOutlined,
   InstagramOutlined,
   WhatsAppOutlined,
+  ArrowRightOutlined,
 } from "@ant-design/icons";
 
 import Signature from "../Icon/Signature.png";
 
-import Img1 from '../Img/woman-planting-plant-in-pot.jpg';
+import Img1 from "../Img/woman-planting-plant-in-pot.jpg";
+import Img2 from "../Img/AboutUs img 1.webp";
+import Img3 from "../Img/AboutUs img 2.jpg";
+import Img4 from "../Img/AboutUs img 3.png";
+
+import { Link } from "react-router-dom";
 
 export default function AboutUs({ hide }) {
   return (
-    <div style={{ width: "100%" }}>
+    <div style={{ width: "100%", backgroundColor: "#fbfbfb" }}>
       {!hide && <Header />}
       <div className="AboutUs-Body">
         <div className="AboutUs-Left">
-          <img className="AboutUs-Left-Img" src={Img1} alt=""/>
+          <img className="AboutUs-Left-Img" src={Img1} alt="" />
         </div>
 
         <div className="AboutUs-Right">
@@ -73,7 +79,55 @@ export default function AboutUs({ hide }) {
         </div>
       </div>
 
-      <div className="AboutUs-Box">hhh</div>
+      <div className="AboutUs-Box">
+        <div className="AboutUs-Box2">
+          <img className="AboutUs-Box-Img" src={Img3} alt="" />
+          <img className="AboutUs-Box-Img" src={Img2} alt="" />
+        </div>
+
+        <div className="AboutUs-Box3">
+          <div className="AboutUs-Box4">
+            <p className="AboutUs-Box-Text1">Plants Care Instruction</p>
+            <p className="AboutUs-Box-Text2">
+              Each order includes a small care guide booklet.
+            </p>
+            <Link className="AboutUs-Box-Link">
+              <p className="AboutUs-Box-Text3">Shop Now</p>
+              <ArrowRightOutlined className="AboutUs-Box-Icon" />
+            </Link>
+          </div>
+
+          <div className="AboutUs-Box4">
+            <p className="AboutUs-Box-Text1">Editor's Pick</p>
+            <p className="AboutUs-Box-Text2">
+              Find the best plants hand-picked by our editor.
+            </p>
+            <Link className="AboutUs-Box-Link">
+              <p className="AboutUs-Box-Text3">Shop Now</p>
+              <ArrowRightOutlined className="AboutUs-Box-Icon" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="AboutUs-Box5">
+          <div className="AboutUs-Box6">
+            <p className="AboutUs-Text2" >Buy Online Now & Get 10% Off!</p>
+            <p className="AboutUs-Text3">
+              Curae quia enim aute, consequatur, eius quaerat excepteur fames
+              pharetra.
+            </p>
+
+            <Link to="/Shop">
+              <div className="AboutUs-Home-Box2">
+                <p className="AboutUs-Home-Text3">Shop Now</p>
+              </div>
+            </Link>
+          </div>
+
+          <img className="AboutUs-Box-Img2" src={Img4} alt="" />
+        </div>
+      </div>
+      <div style={{height:'30px', backgroundColor: "#fbfbfb" }}/>
       {!hide && <Footer />}
     </div>
   );
