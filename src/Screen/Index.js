025c51cf13add641192ef300dd4d4b07d794311a
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-25 01:26:44 +0600

    ok

--- src/Screen/Index.js
+++ src/Screen/Index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Index.css";
+import "./Home.css";
 
 import { Link } from "react-router-dom";
 import { ArrowRightOutlined } from "@ant-design/icons";
+
+import ProductsCart from "../Cart/ProductsCart";
 
 import Icon1 from "../Icon/plant.png";
 import Icon2 from "../Icon/plant (1).png";
@@ -13,6 +16,11 @@ import Img1 from "../Img/Cactus.jpeg";
 import Img2 from "../Img/Bonsai.jpeg";
 import Img3 from "../Img/Succulent.jpeg";
 import Img4 from "../Img/Indoor Plants.jpeg";
+
+import Img5 from "../Img/img-1.jpeg";
+import Img6 from "../Img/img-2.jpeg";
+import Img7 from "../Img/img-3.jpeg";
+import Img8 from "../Img/img-4.jpeg";
 
 export default function Index() {
   return (
@@ -118,6 +126,73 @@ export default function Index() {
           </div>
         </div>
       </div>
+
+      <div className="Index-Spec" />
+      <div className="Index-Spec" />
+
+      <div className="Index-Box3">
+        <p className="Index-Box3-Text1">Featured Products</p>
+
+        <div className="Index-Box3-Div1">
+          <p className="Index-Box3-Text2">
+            Use these paragraphs to focus on the topic in the headline. Make
+            sure you keep it short and attractive.
+          </p>
+
+          <Link to="/">
+            <div className="Index-Box3-Div2">
+              <p className="Index-Box3-Text3">View All Plants</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className="Index-Box3-Div3">
+          <div className="Index-Src-Mb">
+            <ProductsCart
+              image={Img5}
+              name="Lemon Bonsai"
+              price="150.00"
+              rating={0}
+              percentage="20"
+              shippingFee="50"
+              category="Bonsai"
+            />
+
+            <ProductsCart
+              image={Img6}
+              name="Rubber Indoor Plant"
+              price="45.00"
+              rating={0}
+              percentage="0"
+              shippingFee="50"
+              category="Indoor Plant"
+            />
+          </div>
+
+          <div className="Index-Src-Mb">
+            <ProductsCart
+              image={Img7}
+              name="Boncellensis Secullant"
+              price="22.00"
+              rating={0}
+              percentage="0"
+              shippingFee="50"
+              category="Succulent"
+            />
+
+            <ProductsCart
+              image={Img8}
+              name="Old Lady Cactus"
+              price="28.00"
+              rating={0}
+              percentage="0"
+              shippingFee="50"
+              category="Cactus"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="Index-Spec" />
       <div className="Index-Spec" />
     </div>
   );
