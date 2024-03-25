b3dd310c68aed11f40d6f8dc509e288cd3b907c9
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-25 06:45:12 +0600

    ok

--- src/Components/BuyOnline.js
+++ src/Components/BuyOnline.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import './BuyOnline.css';
+import React from "react";
+import "./BuyOnline.css";
+
+import Img1 from "../Img/App Disigen.png";
+
+import Icon1 from "../Icon/App store logo.png";
+import Icon2 from "../Icon/play soter.png";
 
 export default function BuyOnline() {
   return (
-    <div>BuyOnline</div>
-  )
+    <div className="Online-Body">
+      <img className="Online-Img" src={Img1} alt="" />
+
+      <div className="Online-Div">
+        <p className="Online-Text1">Download The App Now!</p>
+        <p className="Online-Text2">For better experience download our app!</p>
+
+        <div className="Online-Div2">
+          <img className="Online-Img2" src={Icon2} alt="" />
+          <img className="Online-Img3" src={Icon1} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
